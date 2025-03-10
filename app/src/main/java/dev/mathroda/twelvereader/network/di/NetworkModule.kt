cb2807825c201893335cd49a5c47b7e84f58f8f9
MathRoda <mathrodabusiness@gmail.com>
2025-03-10 14:09:35 +0200

    Rocky mistake lol

--- app/src/main/java/dev/mathroda/twelvereader/network/di/NetworkModule.kt
+++ app/src/main/java/dev/mathroda/twelvereader/network/di/NetworkModule.kt
@@ -14,7 +14,7 @@ import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 
 private const val BASE_URL = "https://api.elevenlabs.io"
-private val API_KEY: String = "sk_1277d7bdde90c282a4bd8dc9b3f41aa5acb1be2c9145bcf4" //TODO("Please enter your API Key")
+private val API_KEY: String = TODO("Please enter your API Key")
 
 val networkModule = module {
     val engine = OkHttp.create {
